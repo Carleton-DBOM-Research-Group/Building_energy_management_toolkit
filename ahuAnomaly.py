35856adcf99617f846372ad5d1fa2d9a027e5084
Andre Markus <andremarkus@cmail.carleton.ca>
2021-09-24 13:51:51 -0400

    reinstate Agg from tkAgg

--- ahuAnomaly.py
+++ ahuAnomaly.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib
-matplotlib.use('tkAgg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn import linear_model, cluster,metrics,mixture
 from sklearn.decomposition import PCA
